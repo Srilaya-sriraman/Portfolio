11f5b1a1de09516e9423000bb1371d1c98fc9eda
Srilaya-sriraman <srilaya.sriraman2022@vitstudent.ac.in>
2024-05-20 13:58:24 +0530

    update 10

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
   return (
     <section className={styles.container} id="contact">
         <div className={styles.content}>
-            <a href="#"><img src={getImageUrl("LinkedIn.png")} className={styles.img}></img></a>
-            <a href="#"><img src={getImageUrl("Github.png")} className={styles.img}></img></a>
-            <a href="#"><img src={getImageUrl("Mail.png")} className={styles.img}></img></a>
+            <a href="https://in.linkedin.com/in/srilaya-sriraman-75249b252?original_referer=https%3A%2F%2Fwww.google.com%2F"><img src={getImageUrl("LinkedIn.png")} className={styles.img}></img></a>
+            <a href="https://github.com/Srilaya-sriraman"><img src={getImageUrl("Github.png")} className={styles.img}></img></a>
+            <a href="mailto:srilayasriraman@gmail.com"><img src={getImageUrl("Mail.png")} className={styles.img}></img></a>
         </div>
         <p className={styles.desp}>Made with Love by Srilaya ❤️</p>
     </section>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,37 +11,37 @@ const Projects = () => {
                 <img src={getImageUrl("Tic-tac-toe.png")} className={styles.img} width="200px"></img>
                 <p className={styles.head}>Tic-Tac-Toe</p>
                 <p className={styles.desp}>Interactive tic-tac-toe game built using HTMl, CSS and JavaScript</p>
-                <a href="#" className={styles.btn}>To view</a>
+                <a href="https://srilaya-sriraman.github.io/Tic-Tac-Toe/" className={styles.btn}>View</a>
             </div>
             <div className={styles.project}>
                 <img src={getImageUrl("Srilaya's Porfolio.png")} className={styles.img} width="200px"></img>
                 <p className={styles.head}>My portfolio page</p>
                 <p className={styles.desp}>Responsive and adaptive page that showcases my Vite React js skills</p>
-                <a href="#" className={styles.btn}>To view</a>
+                <a href="#" className={styles.btn}>View</a>
             </div>
             <div className={styles.project}>
                 <img src={getImageUrl("FarmToFactory.png")} className={styles.img} width="200px"></img>
                 <p className={styles.head}>FarmToFactory</p>
                 <p className={styles.desp}>An solid user responsive and interactive design for an e-commerce website that lets farmers sell their waste products online.( Using Figma)</p>
-                <a href="#" className={styles.btn}>To view</a>
+                <a href="https://www.figma.com/design/N6BNstcy3KBiCzjM0htpuo/FarmToFactory?node-id=0%3A1&t=AJwPNOlBafojoDfz-1" className={styles.btn}>View</a>
             </div>
             <div className={styles.project}>
                 <img src={getImageUrl("HumanoidX.png")} className={styles.img} width="200px"></img>
                 <p className={styles.head}>HumanoidX</p>
                 <p className={styles.desp}>Design for HumanoidX's official club website. Contains many interesting features and has been implemented. (Using Figma)</p>
-                <a href="#" className={styles.btn}>To view</a>
+                <a href="https://www.figma.com/design/ajjXNQ0Nwx1XTWnieqjMXa/Untitled?node-id=83%3A576&t=bllvj9MhonkfF2Ev-1" className={styles.btn}>View</a>
             </div>
             <div className={styles.project}>
                 <img src={getImageUrl("Bloggistar.png")} className={styles.img} width="200px"></img>
                 <p className={styles.head}>Bloggistar</p>
                 <p className={styles.desp}>Design for an interactive and user friendly blogging website (Using Figma)</p>
-                <a href="#" className={styles.btn}>To view</a>
+                <a href="https://www.figma.com/design/LQpxiVqGDgwBXDa0sBoRJd/Blog-Application?node-id=0%3A1&t=bllvj9MhonkfF2Ev-1" className={styles.btn}>View</a>
             </div>
             <div className={styles.project}>
                 <img src={getImageUrl("VMA.png")} className={styles.img} width="200px"></img>
                 <p className={styles.head}>Virtual Medical Assistant</p>
-                <p className={styles.desp}>Patient, Doctor management system using python and MySQL includes remote mail sending</p>
-                <a href="#" className={styles.btn}>To view</a>
+                <p className={styles.desp}>Patient, Doctor management system using python and MySQL that includes remote mail sending</p>
+                <a href="https://github.com/Srilaya-sriraman/Virtual-Medical-Assistant" className={styles.btn}>View</a>
             </div>
         </div>
         <p className={styles.justUpper}> .</p>
