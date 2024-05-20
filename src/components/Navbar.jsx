9ac1f3addc973fa8ed00f38551afee217c76b95d
Srilaya-sriraman <srilaya.sriraman2022@vitstudent.ac.in>
2024-05-20 13:05:35 +0530

    Nav bar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
         <img className={`${styles.menuBtn} ${menuOpen && styles.menuOpenp}`} src={ getImageUrl("hamburg.png")} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)}></img>
             <ul className={`${styles.menuitems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
                 <li> <a href="#about">About</a></li>
-                <li> <a href="#experience">Skill set</a></li>
+                <li> <a href="#experience">Skill Set</a></li>
                 <li> <a href="#proj">Project</a></li>
                 <li> <a href="#contact">Contact</a></li>
             </ul>
